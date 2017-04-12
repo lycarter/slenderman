8fbf3bff52c5841b43ba7a4d538b319fbe96038c
Landon Carter <lcarter@mit.edu>
2017-04-12 16:32:10 -0400

    slenderman working!

--- slender.py
+++ slender.py
@@ -12,26 +12,15 @@ def read_file(f):
         f: An image file
 
     Returns:
-        An m x n 2-D array which contains True for filled pixels and False otherwise.
+        An m x n 2-D array which contains True for filled pixels and False
+        otherwise.
     """
     pix = f.load()
     dims = f.size
 
-    # assume the top-left pixel is empty
+    # assume the top-left pixel is empty, set it as the value to compare against
     empty = pix[0, 0]
-    # print empty
     return [[pix[i,j] != empty for j in range(dims[1])] for i in range(dims[0])]
-
-def dist(p1, p2):
-    """2D distance function.
-
-    Args:
-        p1: two-element tuple
-        p2: two-element tuple
-
-    Returns: the distance from p1 to p2.
-    """
-    return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**0.5
 
 def add_arc(i, j, slender):
     """Adds the arc from (i,j) to the segment, centered at the two segment ends.
@@ -45,9 +34,10 @@ def add_arc(i, j, slender):
         Nothing, but modifies slender in-place to add the arcs.
     """
     # print('add arc called for (%s, %s)' % (i, j))
-    r1 = dist((0, len(slender[0])), (i, j))
-    r2 = dist((len(slender), len(slender[-1])), (i, j))
-    # print('r1, r2: %s, %s' % (r1, r2))
+    r1 = np.linalg.norm(np.array((0, len(slender[0]))) -
+                        np.array((i, j)))
+    r2 = np.linalg.norm(np.array((len(slender), len(slender[-1]))) -
+                        np.array((i, j)))
 
 
     for a in range(i, len(slender)):
@@ -56,8 +46,7 @@ def add_arc(i, j, slender):
         b = int((r1**2 - a**2)**0.5)
         if b >= len(slender[0]):
             break
-        # print("marking (%s, %s) as true" % (a, b))
-        slender[a][b] = True
+        slender[a][len(slender[0]) - b - 1] = True
 
     for a in range(i, -1, -1):
         if (len(slender) - a) > r2:
@@ -65,7 +54,7 @@ def add_arc(i, j, slender):
         b = int((r2**2 - (len(slender) - a)**2)**0.5)
         if b >= len(slender[0]):
             break
-        slender[a][b] = True
+        slender[a][len(slender[0]) - b - 1] = True
 
 def waterfall_fill(slender):
     """Fills in all pixels below any true pixels (makes the shape contiguous).
@@ -77,33 +66,31 @@ def waterfall_fill(slender):
         Nothing, but modifies slender in-place to contiguos-ify it.
     """
     for i in range(len(slender)):
+        fill_in = False
         for j in range(len(slender[i])):
-            fill_in = False
             if slender[i][j]:
                 fill_in = True
             if fill_in:
                 slender[i][j] = True
 
-def convert_rgb(slender):
+def convert_rgb(slender, out_pixels):
     """Converts slender true/false pixels to RGB values.
 
     Args:
         slender: a 2D array of boolean occupancy.
+        out_pixels: Image.pixels of the output image.
 
     Returns:
-        Nothing, but turns slender into an array with the same dimensions,
-    with each True value replaced by a white pixel, and each false value
-    replaced with a black pixel.
+        Nothing, but copies slender values to out_pixels
+        (True -> white, False -> black)
     """
-    print("slender is %s x %s and the inner type is %s" % (len(slender), len(slender[0]), type(slender[0][0])))
+
     for i in range(len(slender)):
         for j in range(len(slender[i])):
             if slender[i][j]:
-                slender[i][j] = 255
+                out_pixels[i, j] = (255, 255, 255)
             else:
-                slender[i][j] = 0
-
-    print("slender is %s x %s and the inner type is %s" % (len(slender), len(slender[0]), type(slender[0][0])))
+                out_pixels[i, j] = (0, 0, 0)
 
 
 def slender_hull(pixels):
@@ -116,37 +103,29 @@ def slender_hull(pixels):
         A 2D array of boolean occupancy, stating the slender hull
     """
     slender = [[False for j in range(len(pixels[0]))] for i in range(len(pixels))]
-    # print "before add_arc"
-    # for col in slender:
-        # print col
+
     for i in range(len(pixels)):
         for j in range(len(pixels[0])):
             if pixels[i][j]:
                 add_arc(i, j, slender)
-                # print "after add_arc"
-                # for col in slender:
-                    # print col
                 slender[i][j] = True
     waterfall_fill(slender)
-    # print "after waterfall fill"
-    # for col in slender:
-        # print col
     return slender
 
 
-
 if __name__ == '__main__':
-    if len(sys.argv) < 1:
-        print "please provide an input filename"
+    if len(sys.argv) < 2:
+        print "please provide input and output filenames"
     input_filename = sys.argv[1]
-    output_filename = sys.argv[2] if len(sys.argv) > 2 else input_filename + '.out'
+    output_filename = sys.argv[2]
 
     f = Image.open(input_filename)
     pixels = read_file(f)
-    # for col in pixels:
-        # print col
-    slender_hull(pixels)
-    convert_rgb(pixels)
-    pixels = np.array(pixels)
-    f_out = Image.fromarray(pixels, 'L')
-    f_out.save(output_filename)
+    pixels = slender_hull(pixels)
+
+    img = Image.new('RGB', (len(pixels), len(pixels[0])), "black")
+    out_pixels = img.load()
+    convert_rgb(pixels, out_pixels)
+
+    img.show()
+    img.save(output_filename)
